01a74218d77bedaf1601f4f0b4dfe407448d029d
Mateus Cruz <fdsmeuirmao2@outlook.com>
2022-09-19 07:08:36 -0300

    Projeto pronto // numero de likes mudando ao clique

--- src/Posts.js
+++ src/Posts.js
@@ -2,16 +2,24 @@ import React from "react"
 
 function PostsLayout(props) {
 
+  function darLikePost(){
+    setLike("Heart")
+    setCor("vermelho")
+  }
+
+
   function darLike(){
     if(likeselecionado === false ){
     setLike("Heart")
     setCor("vermelho")
     setSelec(true)
+    setCurtidas(parseFloat(props.curtidas) + 1)
     }
     else if(likeselecionado === true){
       setLike("Heart-outline")
       setCor("")
       setSelec(false)
+      setCurtidas(parseFloat(props.curtidas))
     }
   }
 
@@ -23,8 +31,7 @@ function PostsLayout(props) {
   const [likezinho, setLike] = React.useState("Heart-outline")
   const [cor, setCor] = React.useState("")
   const [salvo, setSalvo] = React.useState("bookmark-outline")
-
-
+  const [curtidas, setCurtidas] = React.useState(props.curtidas)
 
   return (
       <div class="post">
@@ -39,7 +46,7 @@ function PostsLayout(props) {
         </div>
 
         <div class="conteudo">
-          <img src={props.imagempost} onClick={darLike} />
+          <img src={props.imagempost} onClick={darLikePost} />
         </div>
 
         <div class="fundo">
@@ -57,7 +64,7 @@ function PostsLayout(props) {
           <div class="curtidas">
             <img src={props.imagemcurtida} />
             <div class="texto">
-              Curtido por <strong>{props.usuariocurtida}</strong> e <strong>outras{props.curtidas} pessoas</strong>
+              Curtido por <strong>{props.usuariocurtida}</strong> e <strong>outras{curtidas} pessoas</strong>
             </div>
           </div>
         </div>
@@ -75,8 +82,8 @@ export default function Posts() {
 
 
   const postagens = [
-    {imagemusuario: "assets/img/meowed.svg", usuario: "meowed" , imagempost: "assets/img/gato-telefone.svg"  , imagemcurtida: "assets/img/respondeai.svg" ,  usuariocurtida: "respondeai" , curtidas: "101.523"},
-    {imagemusuario: "assets/img/barked.svg", usuario: "barked", imagempost: "assets/img/dog.svg" , imagemcurtida: "assets/img/adorable_animals.svg" ,  usuariocurtida: "adorable_animals" , curtidas: "99.159" }
+    {imagemusuario: "assets/img/meowed.svg", usuario: "meowed" , imagempost: "assets/img/gato-telefone.svg"  , imagemcurtida: "assets/img/respondeai.svg" ,  usuariocurtida: "respondeai" , curtidas: "101523"},
+    {imagemusuario: "assets/img/barked.svg", usuario: "barked", imagempost: "assets/img/dog.svg" , imagemcurtida: "assets/img/adorable_animals.svg" ,  usuariocurtida: "adorable_animals" , curtidas: "99159" }
   ]
 
   return (
